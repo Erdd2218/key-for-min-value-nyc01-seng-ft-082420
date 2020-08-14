83d1915b4cfe73b6f3b505274ca4c3e2f54b0ab6
Andre Pierre <andre.laurence94@gmail.com>
2020-08-14 23:09:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,8 +11,9 @@ def key_for_min_value(name_hash)
   name_hash.each do |k, v|
  
   if  (v < lowest_num) 
-    small_hash = k 
     lowest_num = v
+    small_hash = k 
+    
     end 
   end 
  small_hash
