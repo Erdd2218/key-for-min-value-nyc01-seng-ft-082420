25c17d3bff52534a187f8bd5ea8f6ca75688e53c
Andre Pierre <andre.laurence94@gmail.com>
2020-08-14 22:42:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,16 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+small_hash = {}
 
 def key_for_min_value(name_hash)
-
+  
+  low_num = Math.floor(Math.random() * 10)
+  lowest_num Math.floor(Math.random())
+name_hash.each do |k, v|
+ 
+  if  v < lowest_num
+    small_hash = k 
+    end 
+  end 
+ small_hash
 end
